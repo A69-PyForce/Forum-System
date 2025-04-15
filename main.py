131442d2ddf765356a86122943bc3f852e1d4cb2
Emanuil <ekstremerlgl@gmail.com>
2025-04-15 12:48:23 +0300

    Added the db schema, other minor changes

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+from fastapi import FastAPI
+from routers.users import router as users_router
+from utils.console_messages import console_message_log
+import uvicorn
+
+app = FastAPI()
+
+# Homepage
+@app.get('/')
+def homepage():
+    return {"message": "Hello World!"}
+
+app.include_router(users_router, prefix="/users", tags=["Users"])
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", port=8000, reload=True)
+    print(*console_message_log(), sep="\n")
