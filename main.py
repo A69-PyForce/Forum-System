135ed43c4d85526e3c743502aba96a0273c85832
Emanuil Hadzhivasilev <107561425+ExtremerBG@users.noreply.github.com>
2025-05-16 11:47:44 +0300

    Added favicon browser endpoint and ico

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse("static/images/favicon.ico")
 
 # =============================== API ROUTERS ===============================
 from routers.api.users_router import api_users_router
